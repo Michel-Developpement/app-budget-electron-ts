ad5d9d49a6d66f7e07225d4087554a08e4504ee7
antonin michel <a.copiercoller@gmail.com>
2023-08-21 16:36:10 +0200

    first commit

---
+++ dist/main.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+let mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    mainWindow.loadFile("index.html");
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+electron_1.app.on("ready", createWindow);
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+//# sourceMappingURL=main.js.map
